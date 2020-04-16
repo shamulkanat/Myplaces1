a5ea4fcf72eaba432b0f7c4a76a20cef8ae2201f
Natallia <tarasevich.natallia@gmail.com>
2020-04-16 17:45:51 +0300

    Initial Commit

---
+++ Myplaces1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Myplaces1
+//
+//  Created by Natallia Tarasevich on 4/16/20.
+//  Copyright © 2020 Natallia Tarasevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
