f7697fe625d9652b89a567132df89c1324dda221
Natallia <tarasevich.natallia@gmail.com>
2020-04-21 14:48:45 +0300

    App Icon Launch Screen Row Hight

--- MainViewController.swift
+++ MainViewController.swift
@@ -10,26 +10,39 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
+    let placesNames = ["Балкан Гриль", "Бочка", "Вкусные истории", "Дастархан", "Индокитай", "Классик", "Шок", "Bonsai", "Burger Heroes", "Kitchen", "Love&Life", "Morris Pub", "Sherlock Holmes", "Speak Easy", "X.O"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+
+        
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return placesNames.count
+
+    }
+        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = placesNames[indexPath.row]
+        cell?.imageView?.image = UIImage(named: placesNames[indexPath.row])
+        cell?.imageView?.layer.cornerRadius =  85/2
+        cell?.imageView?.clipsToBounds = true
+
+        return cell!
+        
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+        
     }
 
     /*
