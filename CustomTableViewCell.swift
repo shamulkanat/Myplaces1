bdcc3e792d7d592ef93031946f6ad512f896e0dc
Natallia <tarasevich.natallia@gmail.com>
2020-04-23 12:40:47 +0300

    Add class CustomTableViewCell

---
+++ CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Myplaces1
+//
+//  Created by Natallia Tarasevich on 4/23/20.
+//  Copyright © 2020 Natallia Tarasevich. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MainViewController.swift
+++ MainViewController.swift
@@ -29,10 +29,10 @@ class MainViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         
-        cell?.textLabel?.text = placesNames[indexPath.row]
-        cell?.imageView?.image = UIImage(named: placesNames[indexPath.row])
-        cell?.imageView?.layer.cornerRadius =  85/2
-        cell?.imageView?.clipsToBounds = true
+        cell!.textLabel?.text = placesNames[indexPath.row]
+        cell!.imageView?.image = UIImage(named: placesNames[indexPath.row])
+        cell!.imageView?.layer.cornerRadius = cell!.frame.size.height / 2
+        cell!.imageView?.clipsToBounds = true
 
         return cell!
         
