e152dbfd7e507ba05e9004bd461825f6d9cc178d
Natallia <tarasevich.natallia@gmail.com>
2020-05-08 17:25:25 +0300

    Add NewPlaceController
    A lot of changes with interface of screen

--- MainViewController.swift
+++ MainViewController.swift
@@ -104,5 +104,6 @@ class MainViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {}
+    
 }
